e836f11fb9113ff9c1c983d5c9d335b0994dc035
Delonsk <kuzmisinr@gmail.com>
2022-04-01 20:58:07 +0200

    Implemented Landing Screen
    Implemented Landing Screen XML, changed layout from constraint to relative and added message TextView.

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/LandingScreenActivity.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/LandingScreenActivity.java
@@ -4,18 +4,28 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LandingScreenActivity extends Activity {
     private Button button;
     private Button login;
+    private TextView messageText;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_screen);
         addListenerOnButton();
+
+        messageText = (TextView) findViewById(R.id.message_text);
+
+        //This is just to make a part of the text bold
+        //No, there is literally no other way, I'm surprised as well
+        String message = "You <b>Are</b> Signed In";
+        messageText.setText(Html.fromHtml(message));
 
         login = (Button) findViewById(R.id.login_button);
 
