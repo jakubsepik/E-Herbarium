dd79e4a150a32cf2cd2b3972316246c50ae1de72
alcrid aka acrid aka jakub <jkomanicky@gmail.com>
2022-04-02 20:19:08 +0200

    password reset option
    made it possible to reset your password

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/LoginActivity.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/LoginActivity.java
@@ -1,8 +1,11 @@
 package sk.spse.oursoft.android.e_herbarium;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +15,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText email, password;
     private Button btnLogin;
     private TextView textRegister;
+    private TextView resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +62,44 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
+
+        resetPassword = (TextView) findViewById(R.id.reset_password);
+        Dialog passwordResetDialog = new Dialog(this);
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passwordResetDialog.setContentView(R.layout.reset_password);
+
+
+                EditText resetArea = (EditText) passwordResetDialog.findViewById(R.id.resetArea);
+                Button resetPasswordButton = (Button) passwordResetDialog.findViewById(R.id.resetPasswordButton);
+
+                resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mAuth.sendPasswordResetEmail(resetArea.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getApplicationContext(),"Password reset email send successfully",Toast.LENGTH_SHORT).show();
+                                passwordResetDialog.dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(),"Error sending reset password mail",Toast.LENGTH_SHORT).show();
+                                passwordResetDialog.dismiss();
+
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.show();
+            }
+        });
+
+}
 
     private void login() {
         String user = email.getText().toString().trim();
