cc8547586ee32610fb1c381ab33b40bacd35d7c4
alcrid aka acrid aka jakub <jkomanicky@gmail.com>
2022-04-04 12:02:00 +0200

    shows when logged in
    made it so that it shows when someone is logged in

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/LandingScreenActivity.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/LandingScreenActivity.java
@@ -6,15 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< Updated upstream
+=======
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import sk.spse.oursoft.android.e_herbarium.database_objects.Plant;
+import sk.spse.oursoft.android.e_herbarium.database_objects.User;
+>>>>>>> Stashed changes
 
 public class LandingScreenActivity extends Activity {
     private Button button;
     private Button login;
+<<<<<<< Updated upstream
+=======
+    private Plant plant;
+    private FirebaseDatabase database ;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef ;
+    private static final String TAG = "MyActivity";
+>>>>>>> Stashed changes
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_screen);
+<<<<<<< Updated upstream
+=======
+        mAuth = FirebaseAuth.getInstance();
+
+        TextView login_status_text = (TextView) findViewById(R.id.login_status_text);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            login_status_text.setText(user.getEmail().split("@")[0]);
+        } else {
+            login_status_text.setText("no user signed in");
+        }
+
+        login = (Button) findViewById(R.id.login_button);
+>>>>>>> Stashed changes
         addListenerOnButton();
 
         login = (Button) findViewById(R.id.login_button);
