0cfbcfcbd765b919a87f7fc9beee560f25bd4d3e
jakubsepik <jakubsepik@gmail.com>
2022-04-06 15:51:03 +0200

    added menu

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/HerbariumViewActivity.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/HerbariumViewActivity.java
@@ -1,16 +1,59 @@
 package sk.spse.oursoft.android.e_herbarium;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class HerbariumViewActivity extends Activity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-    Button button;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HerbariumViewActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.herbarium_view);
+        
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add:
+                addItem();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addItem() {
+
+    }
+
+
+
 }
