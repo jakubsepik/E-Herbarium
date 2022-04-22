fbc0bc47c0aa2785a1215373cf1ccb1f18a56be4
jakubsepik <jakubsepik@gmail.com>
2022-04-22 10:21:03 +0200

    updated logic and implemented template

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/ListLogic.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/ListLogic.java
@@ -1,52 +1,83 @@
 package sk.spse.oursoft.android.e_herbarium;
 
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListLogic {
-    static JSONObject object=null;
+    private JSONObject object=null;
 
-    static void createList(JSONObject newArray) {
-        object = newArray;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public ListLogic(JSONObject newObject, Context context) {
+        if(newObject==null){
+            try {
+                InputStream is = context.getAssets().open("startingTemplate.json");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                object = new JSONObject(new String(buffer, "UTF-8"));
+                Context a=context.getApplicationContext();
+            } catch (IOException | JSONException ex) {
+                ex.printStackTrace();
+
+            }
+        }else
+        object = newObject;
     }
 
-    static void clearAll() {
+    void clearAll() {
         object = null;
     }
 
-    static void addOne(JSONObject object, String category) {
+    void addOne(JSONObject object, String category) {
         try {
-            ((JSONArray) ListLogic.object.get(category)).put(object);
+            ((JSONArray) object.get(category)).put(object);
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    static void deleteOne(int index, String category) {
+    void deleteOne(int index, String category) {
         try {
-            ((JSONArray) ListLogic.object.get(category)).remove(index);
+            ((JSONArray) object.get(category)).remove(index);
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    static void addCategory(String category) {
+    void addCategory(String category) {
 
     }
 
-    static void deleteCategory(int index, String category) {
+
+
+    void deleteCategory(int index, String category) {
         object.remove(category);
     }
 
-    static JSONObject getList() {
+    JSONObject getList() {
         return object;
     }
 
-    static void updateOne() {
+    void updateOne() {
 
     }
 }
