9e501496ddcafb1c5ab014c7dc3de93b44469eea
jakubsepik <jakubsepik@gmail.com>
2022-05-27 10:28:15 +0200

    console clear

--- app/src/main/java/sk/spse/oursoft/android/e_herbarium/herbariumListOperation/Item.java
+++ app/src/main/java/sk/spse/oursoft/android/e_herbarium/herbariumListOperation/Item.java
@@ -35,7 +35,6 @@ public class Item {
     public String toString() {
         StringBuilder output= new StringBuilder();
         for(int i=subItemList.size()-1;i>=0;i--){
-            Log.d("EH", String.valueOf(i));
             if(i==subItemList.size()-1)
                 output.insert(0, subItemList.get(i).toString());
             else
